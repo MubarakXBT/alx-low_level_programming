3bf44afa126531ddf6269838f1bcade979ac8d6c
root <Kazeemmubarak@gmail.com>
2022-09-08 10:48:36 -0700

    C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	float d;
+	char e;
+
+	printf("Size of char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu bytes(s)\nSize of a float: %lu bytes(s)",e,a,b,c,d);
+	return (0);
+}
