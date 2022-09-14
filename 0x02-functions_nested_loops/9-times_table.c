5c33dddc2a7916b8148375f6ec7eac49049c364f
root <Kazeemmubarak@gmail.com>
2022-09-14 09:18:09 -0700

    t

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main,h"
+/**
+ * times_table - entry
+ *
+ * Return: empty
+ */
+void times_table(void)
+{
+	int i;
+	int j = 0;
+	int n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		while (j <= 9)
+		{
+			n = i * j;
+			_putchar
+		}		
+	}
+}
