96894c43ee784ddf683e6e4de025da7b2a326545
root <Kazeemmubarak@gmail.com>
2022-09-14 07:11:33 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * print_to_98 - ENtry point
+ * @n: Function arg
+ *
+ * Return: void (Success)
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	while (i <= 98)
+	{
+		if (i == n)
+			_putchar (i);
+	}
+	i++;
+}
