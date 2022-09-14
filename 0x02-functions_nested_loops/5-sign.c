c850f6281210292302f488b516206b62fcaa4b3a
root <kazeemmubarak@gmail.com>
2022-09-14 09:59:08 +0000

    Task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: integer number
+ *
+ * Return: 1, 0, -1 (Success)
+ */
+int print_sign(int n)
+{
+	int i;
+	int sign;
+
+	for (i = 0; i > 0; i++)
+	{
+		if (i == n)
+			low = 1;
+		printf("+");
+	}
+	for (i = 0; i <= 0; i++)
+	{
+		if (i == n)
+			low = -1;
+		printf("-");
+	}
+	for (i = 0; i = 0;)
+	{
+		if (i == n)
+			low = 0;
+		printf("0")
+	}
+	return (low);
+}
