86577df0bade7a2c7a8d270173c73744d9355767
root <kazeemmubarak@gmail.com>
2022-09-16 14:41:33 +0000

    nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isdigit - Entry
+ * @c: Function argunent
+ *
+ * Return: 1 if digit, 0 if otherwise
+ */
+int _isdigit(int c);
+{
+	int i;
+	int num;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		if (i == c)
+			alpha = 1;
+		else
+			alpha = 0;
+	}
+	return (num);
+}
+
