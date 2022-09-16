6955c40902946b06bb1e6a20d881d82a09ee5efc
root <kazeemmubarak@gmail.com>
2022-09-16 16:30:36 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: first integer
+ * @b: Second integer
+ *
+ * Return: mul (Success)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
