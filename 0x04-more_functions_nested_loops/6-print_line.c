f8a3cbb791df99c9e82e455ec7be575c0e0fd5d0
root <kazeemmubarak@gmail.com>
2022-09-16 19:07:30 +0000

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: function argument
+ *
+ * Return: Void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
