4dabb9b5eb8fac97133ec51e95246b12a712b070
MubarakXBT <kazeemmubarak@gmail.com>
2022-09-20 17:13:46 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen - Entry
+ * @s: argument
+ *
+ * Return: 0 (Success)
+ */
+int _strlen(char *s)
+{
+	s[]="geeks";
+	printf("%d", strlen(s));
+	return (0);
+}
