fa4102cf4c0f053de7546bfcaf032d8f9397595b
MubarakXBT <kazeemmubarak@gmail.com>
2022-09-28 22:15:45 +0100

    recursive 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,11 @@ void _puts_recursion(char *s)
 {
 
 	if (*s == '\0')
-		return;
-	if (*s)
 	{
+		_putchar('\n');
+		return;
+	}
 		_putchar (*s);
 		s++;
 		_puts_recursion(s);
-	}
-	return;
-	_putchar ('\n');
 }
