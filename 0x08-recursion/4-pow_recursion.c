2c55418345562a8d183d1cbee2ddceaef1ebc275
MubarakXBT <kazeemmubarak@gmail.com>
2022-09-29 07:39:06 +0100

    recursive 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - Function
+ * @x: parameter
+ * @y: argument
+ *
+ * Return: -1 or 1 or "x" or "result"
+ */
+int _pow_recursion(int x, int y)
+{
+	int res;
+
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	res = x * _pow_recursion(x, (y -1));
+	return (res);
+}
