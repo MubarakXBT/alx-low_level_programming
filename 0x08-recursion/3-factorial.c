22f983b4aa2508e814656191457ba4f7e0ca6c59
MubarakXBT <kazeemmubarak@gmail.com>
2022-09-29 07:27:45 +0100

    recursive 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * fatorial - Function
+ * @n: parameter
+ *
+ * Return: -1 oe 1 or res
+ */
+int factorial(int n)
+{
+	int res;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1 || n == 0)
+		return (1);
+
+	res = n * factorial(n - 1);
+	return (res);
+}
