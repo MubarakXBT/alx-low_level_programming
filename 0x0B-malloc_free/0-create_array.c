96732c9c13eeff2fb32cc2267e2e205d924092f7
MubarakXBT <kazeemmubarak@gmail.com>
2022-10-04 15:06:41 +0100

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	s = malloc(size * sizeof(char));
-	if (size == 0)
+	if (size == NULL)
 		return (NULL);
 	if (s == NULL)
 		return (NULL);
