93113d57eae22403c11741de324bceb35362f836
root <kazeemmubarak@gmail.com>
2022-10-10 17:11:29 +0000

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
+ * new_dog - New Function for initialisation
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  *
+ * Return: "User" or "NULL"
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *user;
+
 	user = malloc(sizeof(dog_t));
 
 	if (user == NULL)
