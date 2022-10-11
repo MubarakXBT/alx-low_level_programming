e1083db0ec3bc29b459d7de94d038f4b43a4faf6
root <kazeemmubarak@gmail.com>
2022-10-11 04:24:16 +0000

    Task 4 and 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,13 +11,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *user;
+	char *cpname, *cpown;
+	int lenm = 0, leno = 0, i;
 
+	while(name[lenm])
+		lenm++;
+	while(owner[leno])
+		leno++;
+	
 	user = malloc(sizeof(dog_t));
-
 	if (user == NULL)
 		return (NULL);
-	user->name = name;
+
+	cpname = malloc(lenm + 1);
+	if (cpname == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		cpname[i] = name[i];
+	cpname[i] ='\0';
+
+	cpown = malloc(leno + 1);
+	if (cpown == NULL)
+		return (NULL);
+	for (i = 0; owner[i]; i++)
+		cpown[i] = owner[i];
+	cpown[i] ='\0';
+
+	user->name = cpname;
 	user->age = age;
-	user->owner = owner;
+	user->owner = cpown;
 	return (user);
 }
