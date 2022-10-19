deb3abcd2b89a863714715e096c9303d06ed439d
MubarakXBT <kazeemmubarak@gmail.com>
2022-10-19 14:17:20 +0100

    Variadic function Task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include  <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - Function to print given digits
+ * @separator: Sign to separate different numbers
+ * @n: amount of digits to be printed
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+        unsigned int j = 1, i = 0;
+        char *val;
+        const char *sep;
+        va_list ptr;
+
+        va_start(ptr, n);
+
+        for (sep = separator; i < n; i++)
+        {
+                if (sep != NULL && *sep != '\0')
+                {
+                        val = va_arg(ptr, char *);
+			if (val == NULL)
+				printf("nil");
+			else
+			{
+				printf("%s", val);
+				if (j++ < n)
+				{
+					putchar(*sep);
+					putchar(' ');
+				}
+			}
+                }
+        }
+        putchar('\n');
+}
