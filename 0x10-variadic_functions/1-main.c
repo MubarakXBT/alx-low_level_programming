f2eccbcfea2e624a6f3132f730ad57ac609e8c69
MubarakXBT <kazeemmubarak@gmail.com>
2022-10-19 12:11:52 +0100

    Variadic function Task 2

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers("- ", 8, 0, 98, -1024, 402, 33, 903, 472, 0);
+    return (0);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include  <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - Function to print given digits
+ * @separator: Sign to separate different numbers
+ * @n: amount of digits to be printed
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int j = 1, i = 0;
+	int val;
+	const char *sep;
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (sep = separator; i < n; i++)
+	{
+		if (sep != NULL)
+		{
+			val = va_arg(ptr, int);
+			printf("%d", val);
+			if (j++ < n)
+			{
+				putchar(*sep);
+				putchar(' ');
+			}
+		}
+	}
+}
