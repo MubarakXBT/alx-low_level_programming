7b313dd539610dcd6c63c9b59bb4ada280404398
MubarakXBT <kazeemmubarak@gmail.com>
2023-01-03 17:50:30 +0100

    Simplified

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,6 +9,20 @@ void print_number(int n)
 	int i = n; 
 	if (n >= 0)
 	{
+		if (i > 999999999)
+		{
+			_putchar(n / 1000000000 + '0');
+			_putchar((n / 100000000) % 10 + '0');
+			_putchar((n / 10000000) % 10 + '0');
+			_putchar((n / 1000000) % 10 + '0');
+			_putchar((n / 100000) % 10 + '0');
+			_putchar((n / 10000) % 10 + '0');
+			_putchar((n / 1000) % 10 + '0');
+			_putchar((n / 100) % 10 + '0');
+			_putchar((n / 10) % 10 + '0');
+			_putchar(n % 10 + '0');
+
+		}
 		if (i > 999)
 		{
 			_putchar(n / 1000 + '0');
@@ -32,8 +46,45 @@ void print_number(int n)
 	}
 	else if ( n < 0)
 	{
-		_putchar('-');
-		_putchar((n * -1) / 10 + '0');
-		_putchar((n * -1) % 10 + '0');
+		if (n < -999999999)
+		{
+			_putchar('-');
+			_putchar((n * -1) / 1000000000 + '0');
+			_putchar(((n * -1) / 100000000) % 10 + '0');
+			_putchar(((n * -1) / 10000000) % 10 + '0');
+			_putchar(((n * -1) / 1000000) % 10 + '0');
+			_putchar(((n * -1) / 100000) % 10 + '0');
+			_putchar(((n * -1) / 10000) % 10 + '0');
+			_putchar(((n * -1) / 1000) % 10 + '0');
+			_putchar(((n * -1) / 100) % 10 + '0');
+			_putchar(((n * -1) / 10) % 10 + '0');
+			_putchar(((n % 10) * -1) + '0');
+		}
+		else if (n < -999)
+		{
+			_putchar('-');
+			_putchar((n * -1) / 1000 + '0');
+			_putchar(((n * -1) / 100) % 10 + '0');
+			_putchar(((n * -1) / 10) % 10 + '0');
+			_putchar(((n % 10) * -1) + '0');
+		}
+		else if (n < -99)
+		{
+			_putchar('-');
+			_putchar((n * -1) / 100 + '0');
+			_putchar(((n * -1) / 10) % 10 + '0');
+			_putchar(((n % 10) * -1) + '0');
+		}
+		else if (n < -9)
+		{
+			_putchar('-');
+			_putchar((n * -1) / 10 + '0');
+			_putchar(((n % 10) * -1) + '0');
+		}
+		else
+		{
+			_putchar('-');
+			_putchar(((n % 10) * -1) + '0');
+		}
 	}
 }
