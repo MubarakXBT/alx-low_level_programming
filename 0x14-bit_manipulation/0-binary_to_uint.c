bc627f88007b78b769ec5145a1d46f7070d7345a
MubarakXBT <kazeemmubarak@gmail.com>
2023-02-06 11:38:39 +0100

    Bit Manipulation Task 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * Binary to unit - Converts Binary Number
+ * @b: A pointer to binary string
+ * Return: Result converted
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	Unsigned int base = 1, result - 0, len = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[len])
+		len++;
+
+	while (len)
+	{
+		if (b[len -1] != '0' && b[len -1] != '1')
+			return (0);
+
+		if (b[len -1] == '1')
+			result += base;
+		base *= 2;
+		len--;
+	}
+	return (result);
+}
