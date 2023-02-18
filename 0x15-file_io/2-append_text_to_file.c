35ea10641c17a53a539ddf9c1ef49fb5f9758fcf
MubarakXBT <kazeemmubarak@gmail.com>
2023-02-18 12:46:01 +0100

    File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * append_text_to_file - A program the append text to existing file
+ * @filename: name of file to be edited
+ * @text_content: content to Append
+ * Return: 1 Success or 0 Error
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fdo, fdw, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdo = open(filename, O_RDWR | O_APPEND);
+	if (fdo < 0)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fdo);
+		return (1);
+	}
+
+	while (*(text_content + len))
+		len++;
+
+	fdw = write(fdo, text_content, len);
+	close(fdo);
+	if (fdw < 0)
+		return (-1);
+
+	return (1);
+}
