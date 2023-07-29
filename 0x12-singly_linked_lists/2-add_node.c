d5b593612f8b951c85c2030e5636e0a7232c8d34
MubarakXBT <kazeemmubarak@gmail.com>
2023-07-29 23:11:39 +0000

    Linked_list->Task 2(code worked. betty fix)

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,11 @@
 #include "lists.h"
 #include <string.h>
 /**
+ * add_node - add node at beginnineg of a list
+ * @head: the header of the list to be displaced
+ * @str: the data to be added
+ *
+ * Return: the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
