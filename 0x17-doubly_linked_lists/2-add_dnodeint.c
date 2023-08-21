be0b3317d0d4319104abdb2e6e9abdddeec0c1b7
MubarakXBT <kazeemmubarak@gmail.com>
2023-08-21 18:24:59 +0100

    Low-level Project 0x17: Task 2 [a function That adds a new node to beginning of a Linked-List]

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,6 +3,10 @@
 #include <stddef.h>
 /**
  * add_dnodeint - a function to add to a new-node
+ * @n = value to append in node
+ * @head: head of node
+ *
+ * Return: Newnode
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
@@ -14,5 +18,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	Newnode->prev = NULL;
 	*head = Newnode;
 
-	return (Newnode);
+	return (*head);
 }
