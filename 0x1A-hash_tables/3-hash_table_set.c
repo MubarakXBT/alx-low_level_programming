e09850609974bcde440205fb92ddf12532fd0da6
MubarakXBT <kazeemmubarak@gmail.com>
2023-09-09 20:44:33 +0100

    Task 0: for C - Hash Table [function to insert a new key into the Hash Map]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,10 +24,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	keypair->next = NULL;
 
 	index = (hash_djb2((const unsigned char *)key)) % ht->size;
-
 	if (ht->array[index] != NULL)
 	{
-		return (0);
+		ht->array[index]->next = keypair;
+		return (1);
 	}
 
 	ht->array[index] = keypair;
