d2845718f3bbf68e62248004a50087fbaf2c55d4
MubarakXBT <kazeemmubarak@gmail.com>
2023-09-10 22:38:16 +0100

    Task 0: for C - Hash Table [function to print key and repective pair in the Hash Map] -FIX: print collision using linkrd list

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,37 +1,39 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_print - A function to print hey and pair of an hash map/table
- * @ht: the hash table
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
  *
- * Return: Void
-*/
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
-	int size = ht->size;
-	int i, timing = 0;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
 		return;
 
 	printf("{");
-	for (i = 0; i < size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			if (timing == 1)
-			{
+			if (comma_flag == 1)
 				printf(", ");
-			}
+
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
 				if (node != NULL)
 					printf(", ");
 			}
+			comma_flag = 1;
 		}
 	}
-	puts("}");
+	printf("}\n");
 }
