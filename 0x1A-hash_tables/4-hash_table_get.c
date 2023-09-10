4ca3f010caaceda5b0aeadf4a05c14be1c3accc7
MubarakXBT <kazeemmubarak@gmail.com>
2023-09-10 21:44:45 +0100

    Task 0: for C - Hash Table [function to lookup key in the Hash Map] FIX: index exceed table size i.e collision

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,19 @@
 #include "hash_tables.h"
 /**
+ * hash_table_get - A function to look up a key in a table
+ * @ht: the hash map/table
+ * @key: the key to lookup
  *
+ * Return: the value stored at the key location
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index;
 
 	if (!key || !ht)
+		return (NULL);
+
+	if (index >= ht->size)
 		return (NULL);
 
 	index = hash_djb2((const unsigned char *)key) % ht->size;
