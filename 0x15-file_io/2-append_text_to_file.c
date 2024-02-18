c669a662ad043e46714b84a89df210d4a785794e
MubarakXBT <kazeemmubarak@gmail.com>
2024-02-19 00:44:32 +0100

    File operation to copy content of a file and append to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,32 +6,26 @@
  * @text_content: he NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	register int _file, _write, text_len = 0;
+	int o, w, len = 0;
 
-	if (!(filename))
-	{
+	if (filename == NULL)
 		return (-1);
-	}
-	_file = open(filename, O_WRONLY, O_APPEND);
-	if (_file == -1)
+
+	if (text_content != NULL)
 	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	}
-	if (text_content)
-	{
-		while (text_content[text_len])
-		{
-			text_len++;
-		}
-		_write = write(_file, text_content, text_len);
-		if (_write == -1)
-		{
-			return (-1);
-		}
-	}
-	close(_file);
+
+	close(o);
+
 	return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,85 +1,49 @@
 #include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-int cclose(int);
+
 /**
- * main - main program to copy and past content
- * @ac: argument counter
- * @av: argument variable
- * Return: exit codes to STDERR
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if success
  */
-int main(int ac, char *av[])
+
+int main(int argc, char *argv[])
 {
-	int from_fd = -1, to_fd = -1, error = 0, mem_read = 0, _EOF = 1;
-	char buf[1024];
+	int _file1, _file2, _read, c1, c2;
+	char buffer[1024];
 
-	if (ac != 3)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-
-	from_fd = open(av[1], O_RDONLY);
-	if (from_fd < 0)
+	_file1 = open(argv[1], O_RDONLY);
+	if (_file1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	to_fd = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (to_fd < 0)
+	_file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	while ((_read = read(_file1, buffer, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-
-	while (_EOF)
-	{
-		_EOF = read(from_fd, buf, 1024);
-		if (_EOF < 0)
+		if (_file2 < 0 || (write(_file2, buffer, _read) != _read))
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			cclose(to_fd);
-			cclose(from_fd);
-			exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
-		else if (_EOF == 0)
-			break;
-		mem_read += _EOF;
-		error = write(to_fd, buf, _EOF);
-		if (error < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			cclose(from_fd);
-			cclose(to_fd);
-			exit(98);
-		}
-
 	}
-	error = cclose(to_fd);
-	if (error == 0)
+	if (_read < 0)
 	{
-		cclose(from_fd);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	error = cclose(from_fd);
-		exit(100);
+	c1 = close(_file1);
+	if (c1 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", _file1), exit(100);
+	}
+	c2 = close(_file2);
+	if (c2 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", _file2), exit(100);
+	}
 	return (0);
-}
-
-/**
- * cclose - format error closing of descriptor
- * @arg: File descriptor
- * Return: Error
- */
-int cclose(int arg)
-{
-	int error;
-
-	error = close(arg);
-	if (error < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", arg);
-	return (error);
 }
